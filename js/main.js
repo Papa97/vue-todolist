b6b8b469273a6460bd87f4a70ad770652c8a3e7a
Guido <ppapaguido@gmail.com>
2021-11-29 16:03:55 +0100

    problema push di piu oggetti

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const app = new Vue ({
+    el: '#root',
+    data:{
+        lista : [
+            {
+                text: 'fare la spesa',
+                done: true
+            },
+            {
+                text: 'andare in palestra',
+                done: false
+            },
+            {
+                text: "fare l'esercizio",
+                done: true
+            }
+        ],
+        inputValue: '',
+    },
+
+    methods: {
+        removeToDo: function(i){
+            this.lista.splice(i , 1)
+        },
+        addToDo: function(){
+            this.lista.push(this.inputValue);
+            this.inputValue = '';
+        },
+        
+
+    }
+});
